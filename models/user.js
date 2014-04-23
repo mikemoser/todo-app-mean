ab1115002daa91b5e34f56ec65609cb9931941cd
Mike Moser <mike@humangravity.com>
2014-04-23 16:07:20 -0700

    hashed user password;

--- models/user.js
+++ models/user.js
@@ -3,7 +3,8 @@
 
   var schema = new mongoose.Schema({ 
     username: { type: String, required: true },
-    password: { type: String, required: true } 
+    password: { type: String, required: true }, 
+    salt: { type: String, required: true }
   });
 
   module.exports = mongoose.model('User', schema);
--- services/user.js
+++ services/user.js
@@ -1,28 +1,65 @@
 (function (undefined) {
   var Promise = require('promise');
   var Models = require('../models');
+  var crypto = require('crypto');
 
   function login(username, password) {
     return Promise.denodeify(Models.User.findOne.bind(Models.User))({ username: username })
-    .then(function (result) {
-      // TODO: Check hashed password
-      if (result)
-        return result;
+    .then(function (user) {
+      if (!user)
+        throw new Error('Invalid username or password.');  
+
+      if (!password)
+        throw new Error('Invalid username or password.');
+
+      if (user.password !== hash(user.salt, password))
+        throw new Error('Invalid username or password.');  
       
-      throw new Error('Invalid username or password.');  
+      return user;
     });
   }
 
   function create(username, password) {
+    var hashResult = hashPassword(password);
+
     var user = new Models.User({ 
       username: username,
-      password: password // TODO: Hash password
+      password: hashResult.hash,
+      salt: hashResult.salt
     })
 
     return Promise.denodeify(user.save.bind(user))()
     .then(function (user) {
       return user;
     });
+  }
+
+  function hashPassword(password) {
+    var salt = createSalt();
+    var hashResult = hash(salt, password);
+
+    return {
+      hash: hashResult,
+      salt: salt
+    };
+  }
+
+  function createSalt(length) {
+    length = length ? length : 32;
+
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz';
+    var string = '';
+  
+    for (var i = 0; i < length; i++) {
+      var randomNumber = Math.floor(Math.random() * chars.length);
+      string += chars.substring(randomNumber, randomNumber + 1);
+    }
+
+    return string;
+  }
+
+  function hash(salt, data) {
+    return crypto.createHmac('sha256', salt).update(data).digest('base64');
   }
 
   module.exports = {
